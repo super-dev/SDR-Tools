8c01ac8e112bdc7f84b840f312a4ff5d5d6a420f
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-02-02 19:48:37 +0530

    Folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var jade        = require('gulp-jade');
+var gutil       = require('gulp-util');
 var reload      = browserSync.reload;
+
+var SRC         = './src/';
+var SRC_JADE    = SRC + '**/*.jade';
+var SRC_SCSS    = SRC + 'sass/**/*.scss';
+
+var DEST        = './../htdocs/tools/';
+var DEST_JADE   = DEST;
+var DEST_SCSS   = DEST + 'css';
+
+gutil.log(SRC_JADE);
 
 /**
  * Compile jade files into HTML
@@ -11,11 +22,11 @@ gulp.task('templates', function() {
 
     var YOUR_LOCALS = {};
 
-    return gulp.src('./app/*.jade')
+    return gulp.src(SRC_JADE)
         .pipe(jade({
             locals: YOUR_LOCALS
         }))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest(DEST_JADE))
 });
 
 /**
@@ -28,9 +39,9 @@ gulp.task('jade-watch', ['templates'], reload);
  * Sass task for live injecting into all browsers
  */
 gulp.task('sass', function () {
-    return gulp.src('./app/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/css'))
+    return gulp.src(SRC_SCSS)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(DEST_SCSS))
         .pipe(reload({stream: true}));
 });
 
@@ -39,8 +50,8 @@ gulp.task('sass', function () {
  */
 gulp.task('default', ['sass', 'templates'], function () {
 
-    browserSync({server: './dist'});
+    browserSync.init({proxy: "http://local.superdevresources.dev/tools/", browser: "chrome"});
 
-    gulp.watch('./app/scss/*.scss', ['sass']);
-    gulp.watch('./app/*.jade',      ['jade-watch']);
+    gulp.watch(SRC_SCSS,      ['sass']);
+    gulp.watch(SRC_JADE,      ['jade-watch']);
 });
