a895a81d1db1966e1f0a49aad9fe3d4817bfd5dc
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-02-13 18:07:48 +0530

    HTML and Jade textarea

--- public/js/html-jade.js
+++ public/js/html-jade.js
@@ -6,12 +6,20 @@ require.config({
   }]
 });
 
+var html, jade;
+
 require([
   "codemirror/lib/codemirror",
-  "codemirror/mode/htmlmixed/htmlmixed"
+  "codemirror/mode/htmlmixed/htmlmixed",
+  "codemirror/mode/jade/jade"
 ], function(CodeMirror) {
-  CodeMirror.fromTextArea(document.getElementById("html"), {
+  html = CodeMirror.fromTextArea(document.getElementById("html"), {
     lineNumbers: true,
     mode: "htmlmixed"
+  });
+
+  jade = CodeMirror.fromTextArea(document.getElementById("jade"), {
+    lineNumbers: true,
+    mode: "jade"
   });
 });
