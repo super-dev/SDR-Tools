c0beb9d8e247cf08e81ac8f7d6840eeb52e896b5
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-10-23 19:59:05 +0530

    Category pages for tools

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# SDR Tools
+[![Build Status](https://travis-ci.org/super-dev/SDR-Tools.svg?branch=master)](https://travis-ci.org/super-dev/SDR-Tools)
 
-Tools for Web Developers 
+# [SDR Tools](https://tools.superdevresources.com/)
+
+Free Tools for Web and App Developers by Super Dev Resources Team
+
+## Tools
+
+ - [Image Clipper](https://tools.superdevresources.com/image-cropper/) - Online tool for cropping images
+
+## Credits
+
+ - [MMCSS](https://mmcss.kunruchcreations.com/) - CSS Framework used for design
+ - [mmpilot](https://mmpilot.kunruchcreations.com/) - Build tool
+ - [Font Awesome](http://fontawesome.io/) and [IcoMoon](https://icomoon.io/) for SVG icons used on site.
+
+## License
+
+Licensed under the MIT License
--- common/data/tools.js
+++ common/data/tools.js
@@ -3,25 +3,34 @@ fs   = require('fs');
 path = require('path');
 
 exports.data = function() {
-  var data = []
+  var data = {
+    all: [],
+    design: [],
+    development: [],
+    marketing: []
+  }
   
   // get data path
   var dataPath = path.join(__dirname, '_tools-raw.yml')
   var toolsPath = path.join(__dirname, '../..', 'tools')
 
   try {
-    data = yaml.safeLoad(fs.readFileSync(dataPath, 'utf8'))
-    for(var i = 0; i < data.length; i++) {
-      var toolItem = data[i]
+    data.all = yaml.safeLoad(fs.readFileSync(dataPath, 'utf8'))
+    for(var i = 0; i < data.all.length; i++) {
+      var toolItem = data.all[i]
       toolItem.icon = path.join(toolsPath, toolItem.path, 'assets', toolItem.icon)
       if(path.parse(toolItem.icon).ext === '.svg') {
         toolItem.icon = fs.readFileSync(toolItem.icon, 'utf8')
       }
 
       toolItem.url = ('/' + toolItem.path).replace(/\\/g, '/')
+
+      data[toolItem.category].push(toolItem)
     }
   } catch (e) {
     console.log(e);
   }
+
+  // console.log("DATA: " + JSON.stringify(data))
   return data
 }
