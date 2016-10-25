68335e86ea13274d0f2442167782d1ba48af176a
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-10-25 21:31:29 +0530

    Drag area image load

--- tools/base64-image/scripts/base64-image.js
+++ tools/base64-image/scripts/base64-image.js
@@ -1,48 +1,59 @@
-function el(id){return document.getElementById(id);} // Get elem by ID
+function el(id) { return document.getElementById(id); } // Get elem by ID
 
-var canvas  = el("canvas");
+var canvas = el("canvas");
 var context = canvas.getContext("2d");
 
 
 function readImage() {
-    if ( this.files && this.files[0] ) {
-        var filetype = this.files[0].type;
-      	//	Prevent any non-image file type from being read.
-        if(!filetype.match(/image.*/)){
-          alert("The dropped file is not an image: ", filetype);
-          return;
-        }
+  if (this.files && this.files[0]) {
+    loadImage(this.files[0]);
+  }
+}
 
-        el("load-prompt").innerHTML = "Loading...";
+function loadImage (src) {
+  var filetype = src.type;
+  //	Prevent any non-image file type from being read.
+  if (!filetype.match(/image.*/)) {
+    alert("The dropped file is not an image: ", filetype);
+    return;
+  }
 
-        var FR= new FileReader();
-        FR.onload = function(e) {
-           var img = new Image();
-           img.onload = function() {
-             var containerWidth = el("canvas-container").offsetWidth;
-             //console.log("Container Width: " + containerWidth);
-             if(img.naturalWidth > containerWidth) {
-               img.height = containerWidth * (img.height / img.width);
-               img.width = containerWidth;
-             }
-             canvas.width = img.width;
-             canvas.height = img.height;
-             context.drawImage(img, 0, 0, img.width, img.height);
-             el("click-target").style.display = 'none';
-             el("canvas").style.display = 'block';
+  el("load-prompt").innerHTML = "Loading...";
 
-             // load data uri in text area
-             if(filetype === 'image/png') {
-              el("base64-out").value = canvas.toDataURL();
-             } else {
-              el("base64-out").value = canvas.toDataURL("image/jpeg", 1.0);
-             }  
-             el("result-area").style.display = "block";
-           };
-           img.src = e.target.result;
-        };       
-        FR.readAsDataURL( this.files[0] );
-    }
+  var FR = new FileReader();
+  FR.onload = function (e) {
+    var img = new Image();
+    img.onload = function () {
+      var containerWidth = el("canvas-container").offsetWidth;
+      //console.log("Container Width: " + containerWidth);
+      if (img.naturalWidth > containerWidth) {
+        img.height = containerWidth * (img.height / img.width);
+        img.width = containerWidth;
+      }
+      canvas.width = img.width;
+      canvas.height = img.height;
+      context.drawImage(img, 0, 0, img.width, img.height);
+      el("click-target").style.display = 'none';
+      el("canvas").style.display = 'block';
+
+      // load data uri in text area
+      if (filetype === 'image/png') {
+        el("base64-out").value = canvas.toDataURL();
+      } else {
+        el("base64-out").value = canvas.toDataURL("image/jpeg", 1.0);
+      }
+      el("result-area").style.display = "block";
+    };
+    img.src = e.target.result;
+  };
+  FR.readAsDataURL(src);
 }
 
 el("fileUpload").addEventListener("change", readImage, false);
+
+var target = el("drop-target");
+target.addEventListener("dragover", function (e) { e.preventDefault(); }, true);
+target.addEventListener("drop", function (e) {
+  e.preventDefault();
+  loadImage(e.dataTransfer.files[0]);
+}, true);
--- tools/image-cropper/scripts/image-cropper.js
+++ tools/image-cropper/scripts/image-cropper.js
@@ -2,7 +2,7 @@
 var cropper = null;
 var filename = "download";
 
-function el(id){return document.getElementById(id);} // Get elem by ID
+function el(id) { return document.getElementById(id); } // Get elem by ID
 
 el("select-all").addEventListener('click', select_all);
 
@@ -10,76 +10,76 @@ el("jpg-download").addEventListener('click', download_jpg);
 el("png-download").addEventListener('click', download_png);
 
 var target = el("drop-target");
-target.addEventListener("dragover", function(e){e.preventDefault();}, true);
-target.addEventListener("drop", function(e){
-	e.preventDefault();
-	loadImage(e.dataTransfer.files[0]);
+target.addEventListener("dragover", function (e) { e.preventDefault(); }, true);
+target.addEventListener("drop", function (e) {
+  e.preventDefault();
+  loadImage(e.dataTransfer.files[0]);
 }, true);
 
 el("fileUpload").addEventListener("change", readImage, false);
 
 var rad = document.aspectform.aspectratio;
-for(var i = 0; i < rad.length; i++) {
-    rad[i].onclick = function() {
-        if(cropper != null) {
-					var cropper_data = cropper.getData();
-          if(this.value == "16:9") {
-            cropper.setAspectRatio(16/9);
-						cropper.setData({'x':cropper_data.x, 'y':cropper_data.y});
-          }
-          else if (this.value == "4:3") {
-            cropper.setAspectRatio(4/3);
-						cropper.setData({'x':cropper_data.x, 'y':cropper_data.y});
-          }
-          else  {
-            cropper.setAspectRatio(NaN);
-						free_mode_data(cropper);
-          }
-        }
-    };
+for (var i = 0; i < rad.length; i++) {
+  rad[i].onclick = function () {
+    if (cropper != null) {
+      var cropper_data = cropper.getData();
+      if (this.value == "16:9") {
+        cropper.setAspectRatio(16 / 9);
+        cropper.setData({ 'x': cropper_data.x, 'y': cropper_data.y });
+      }
+      else if (this.value == "4:3") {
+        cropper.setAspectRatio(4 / 3);
+        cropper.setData({ 'x': cropper_data.x, 'y': cropper_data.y });
+      }
+      else {
+        cropper.setAspectRatio(NaN);
+        free_mode_data(cropper);
+      }
+    }
+  };
 }
 
 // Read image from input field
 function readImage() {
-    if ( this.files && this.files[0] ) {
-        loadImage(this.files[0]);
-    }
+  if (this.files && this.files[0]) {
+    loadImage(this.files[0]);
+  }
 }
 
-function loadImage(src){
-	//	Prevent any non-image file type from being read.
-	if(!src.type.match(/image.*/)){
-		alert("The dropped file is not an image: ", src.type);
-		return;
-	}
+function loadImage(src) {
+  //	Prevent any non-image file type from being read.
+  if (!src.type.match(/image.*/)) {
+    alert("The dropped file is not an image: ", src.type);
+    return;
+  }
   el("load-prompt").innerHTML = "Loading...";
 
   filename = src.name;
 
-	//	Create our FileReader and run the results through the render function.
-	var reader = new FileReader();
-	reader.onload = function(e){
-		render(e.target.result);
-	};
-	reader.readAsDataURL(src);
+  //	Create our FileReader and run the results through the render function.
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    render(e.target.result);
+  };
+  reader.readAsDataURL(src);
 }
-function render(src){
-	var image =   el("myimage");
-	image.onload = function(){
+function render(src) {
+  var image = el("myimage");
+  image.onload = function () {
     // hide click target
     el("click-target").style.display = 'none';
 
     cropper = new Cropper(el('myimage'), {
-			zoomOnWheel: false,
+      zoomOnWheel: false,
       viewMode: 1,
       movable: false,
       rotatable: false,
-			ready: function() {
+      ready: function () {
 
         var canvasData = this.cropper.getCanvasData();
         var scale = 1;
-        if(image.naturalWidth > canvasData.width) {
-          scale = canvasData.width/image.naturalWidth;
+        if (image.naturalWidth > canvasData.width) {
+          scale = canvasData.width / image.naturalWidth;
           image.width = canvasData.width;
           this.cropper.scaleX(scale);
           this.cropper.scaleY(scale);
@@ -89,53 +89,53 @@ function render(src){
           this.cropper.setCanvasData(canvasData);
         }
 
-				free_mode_data(this.cropper);
-			},
-      crop: function(data) {
-				el('img-height').innerHTML = Math.round(data.detail.height);
-				el('img-width').innerHTML = Math.round(data.detail.width);
+        free_mode_data(this.cropper);
+      },
+      crop: function (data) {
+        el('img-height').innerHTML = Math.round(data.detail.height);
+        el('img-width').innerHTML = Math.round(data.detail.width);
       }
     });
-	};
-	image.src = src;
+  };
+  image.src = src;
 }
 
 function free_mode_data(cropper) {
-	var cropper_data = cropper.getData();
-	var c_height = 350;
-	if(c_height > cropper_data.height) {
-		c_height = cropper_data.height;
-	}
-	cropper.setData({'x':0, 'y':0, 'width':cropper_data.width, 'height':c_height});
+  var cropper_data = cropper.getData();
+  var c_height = 350;
+  if (c_height > cropper_data.height) {
+    c_height = cropper_data.height;
+  }
+  cropper.setData({ 'x': 0, 'y': 0, 'width': cropper_data.width, 'height': c_height });
 }
 
 
 function select_all() {
-	var container_data = cropper.getContainerData();
-	cropper.setAspectRatio(NaN);
-	cropper.setData({'x':0, 'y':0, 'width':container_data.width, 'height':container_data.height});
+  var container_data = cropper.getContainerData();
+  cropper.setAspectRatio(NaN);
+  cropper.setData({ 'x': 0, 'y': 0, 'width': container_data.width, 'height': container_data.height });
 }
 
 function download_jpg() {
-	if(cropper != null) {
-		var canv = cropper.getCroppedCanvas();
+  if (cropper != null) {
+    var canv = cropper.getCroppedCanvas();
     if (Modernizr.adownload) {
       download(canv.toDataURL("image/jpeg", 0.9), getFileName(cropper) + ".jpg", "image/jpeg");
     } else {
       window.open(canv.toDataURL("image/jpeg", 0.9));
     }
-	}
+  }
 }
 
 function download_png() {
-	if(cropper != null) {
-		var canv = cropper.getCroppedCanvas();
+  if (cropper != null) {
+    var canv = cropper.getCroppedCanvas();
     if (Modernizr.adownload) {
       download(canv.toDataURL(), getFileName(cropper) + ".png", "image/png");
     } else {
-		  window.open(canv.toDataURL());
+      window.open(canv.toDataURL());
     }
-	}
+  }
 }
 
 function getFileName(cropper) {
