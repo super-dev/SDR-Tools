1ff144c04497e99106470aef2ac1fc71d17ef72d
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-10-29 12:49:41 +0530

    Click to copy implementation

--- tools/color-shades/scripts/color-shades.js
+++ tools/color-shades/scripts/color-shades.js
@@ -2,36 +2,36 @@ new Vue({
   el: '#app',
   data: {
     color: '#fafafa',
-    selectedIndex: -1
+    selectedIndex: 4
   },
   computed: {
     previewStyleIn: function () {
       var color = this.color
-      if(this.color.length == 0) {
+      if (this.color.length == 0) {
         color = "#fafafa"
       }
-      
+
       return "background-color: " + tinycolor(color).toRgbString() + ";"
     },
     previewStyleOut: function () {
       return this.shadesMonochrome[this.selectedIndex]
     },
-    colorOut: function() {
+    colorOut: function () {
       var color = tinycolor(this.shadesMonochrome[this.selectedIndex])
       return {
         hex: color.toHexString(),
-        rgb:  color.toRgbString(),
+        rgb: color.toRgbString(),
         hsl: color.toHslString()
       }
     },
-    shadesMonochrome: function() {
+    shadesMonochrome: function () {
       var shades = []
       var color = this.color
-      if(this.color.length == 0) {
+      if (this.color.length == 0) {
         color = "#fafafa"
       }
       var hsl = tinycolor(color).toHsl()
-      for(var i = 9.5; i >= 0.5; i-=1) {
+      for (var i = 9.5; i >= 0.5; i -= 1) {
         hsl.l = 0.1 * i;
         shades.push("background-color: " + tinycolor(hsl).toRgbString())
       }
@@ -39,14 +39,28 @@ new Vue({
     }
   },
   methods: {
-    copy: function(text) {
+    copy: function (text, event) {
+      var selection = window.getSelection()
+      selection.removeAllRanges()
+
+      event.target.setSelectionRange(0,  event.target.value.length)
+
+      // Copy the selection to clipboard.
+      document.execCommand('copy')
+
       swal({
         title: "Copied!",
-        text: "Copied to clipboard: <strong>" + text + "</strong>",
-        timer: 4000,
+        text: "<strong>" + text + "</strong>",
+        timer: 3000,
         showConfirmButton: true,
         html: true
       });
+    },
+    random: function () {
+      this.color = tinycolor.random().toHexString()
     }
+  },
+  created: function () {
+    this.random()
   }
 })
