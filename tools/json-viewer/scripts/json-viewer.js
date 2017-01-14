bb04407bd7dabf11be08cbf91503c7f6ded1784a
Kanishk Kunal <kanishk.kunal@hotmail.com>
2017-01-14 22:08:24 +0530

    JSON rendering

--- tools/json-viewer/scripts/json-viewer.js
+++ tools/json-viewer/scripts/json-viewer.js
@@ -1,1 +1,22 @@
 
+$(function() {
+  $('#json-input').bind('input propertychange', function() {
+    RenderJSONView();
+  });
+  // Display JSON sample on load
+  RenderJSONView();
+});
+
+
+function RenderJSONView() {
+  try {
+    var input = eval('(' + $('#json-input').val() + ')');
+  }
+  catch (error) {
+    return alert("Cannot eval JSON: " + error);
+  }
+  var options = {collapsed: false};
+  $('#json-renderer').jsonViewer(input, options);
+  $('#json-input').height( $('#json-renderer').height());
+  $('body, html').animate({ scrollTop: 0 }, 800);
+};
