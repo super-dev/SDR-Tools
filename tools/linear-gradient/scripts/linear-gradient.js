46def565fed6c21bd0ade206b718cb03604601a0
Kanishk Kunal <kanishk.kunal@hotmail.com>
2017-02-21 14:25:57 +0530

    Adding color preview

--- tools/linear-gradient/scripts/linear-gradient.js
+++ tools/linear-gradient/scripts/linear-gradient.js
@@ -8,6 +8,12 @@ new Vue({
     }
   },
   computed: {
+    primaryColorBackground: function() {
+      return 'background-color: ' + this.primaryColor;
+    },
+    secondaryColorBackground: function() {
+      return 'background-color: ' + this.secondaryColor;
+    },
     gradient: function() {
       return 'background:  linear-gradient(' + this.direction + 'deg, ' + this.primaryColor + ' 0%, ' + this.secondaryColor + ' 100%)'
     }
