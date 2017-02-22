a52ea551d6a10dede8c5555076f134378ac5e61a
Kanishk Kunal <kanishk.kunal@hotmail.com>
2017-02-22 17:00:13 +0530

    Basic Lorem generator

--- tools/dummy-text/scripts/dummy-text.js
+++ tools/dummy-text/scripts/dummy-text.js
@@ -3,8 +3,34 @@ new Vue({
   data() {
     return {
       generator: 'lorem',      
-      words: 750,
-      paragraphs: 3
+      words: 250,
+      paragraphs: 4
+    }
+  },
+  computed: {
+    loremText: function() {
+      var count = 0
+      var text = '<p>'
+      var paraStop = Math.ceil(this.words / this.paragraphs)
+      var nextStop = paraStop
+      while(count < this.words && count < 10000) {
+        var sentence = sentences(1, false, count == 0)
+        text += sentence
+        count += sentence.split(' ').length
+
+        if(count > nextStop) {
+          text += '</p><p>'
+          nextStop += paraStop
+        }
+        
+        console.log(sentence)
+        console.log(count)
+        console.log(nextStop)
+      }
+
+      text += "</p>"
+
+      return text
     }
   }
 })
