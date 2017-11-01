83761c9004dc4fd67ae97b525c8acbf5897c0dba
Kanishk Kunal <kanishk.kunal@hotmail.com>
2017-11-01 15:45:54 +0530

    Adding Whatversion in tools

--- common/data/tools.js
+++ common/data/tools.js
@@ -15,12 +15,21 @@ exports.data = function() {
     data.all = yaml.safeLoad(fs.readFileSync(dataPath, 'utf8'))
     for(var i = 0; i < data.all.length; i++) {
       var toolItem = data.all[i]
-      toolItem.icon = path.join(toolsPath, toolItem.path, 'assets', toolItem.icon)
       if(path.parse(toolItem.icon).ext === '.svg') {
+        toolItem.icon = path.join(toolsPath, toolItem.path, 'assets', toolItem.icon)
         toolItem.icon = fs.readFileSync(toolItem.icon, 'utf8')
       }
+      else {
+        toolItem.icon = '<img src="' + toolItem.icon + '"></img>';
+      }
 
-      toolItem.url = ('/' + toolItem.path).replace(/\\/g, '/')
+      if(!toolItem.url) {
+        toolItem.url = ('/' + toolItem.path).replace(/\\/g, '/')
+      }
+      else {
+        toolItem.target = '_blank'
+      }
+
       var categories = toolItem.categories
 
       categories.forEach(function (category) {
